45055b05eaab3b33124f098661db758735cf370f
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-09-19 21:58:52 +0530

    srm auth

--- backend/Middlewares/AuthValidation.js
+++ backend/Middlewares/AuthValidation.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 const signupValidation = (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().min(3).max(100).required(),
-    email: Joi.string().email().min(5).max(100).required(),
+    email: Joi.string().pattern(/^[a-zA-Z0-9._%+-]+@srmist\.edu\.in$/).required(),
     password: Joi.string().min(8).max(100).required()
   });
 
@@ -17,7 +17,7 @@ const signupValidation = (req, res, next) => {
 
 const loginValidation = (req, res, next) => {
   const schema = Joi.object({
-    email: Joi.string().email().min(5).max(100).required(),
+    email: Joi.string().pattern(/^[a-zA-Z0-9._%+-]+@srmist\.edu\.in$/).required(),
     password: Joi.string().min(1).required()
   });
 
